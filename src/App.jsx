6a0cc91a2fb8111ead630941393dc18cc72f3238
Sezgi Altan <sezgialtan@Sezgis-Mac.local>
2022-07-30 00:10:17 +0300

    Create :: App Component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Card from "./components/card/card.component";
+
+function App() {
+  return (
+    <div className="App w-full h-screen bg-gradient-to-r from-[#F9E7FE] to-[#DAFCFC] flex items-center justify-center ">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
